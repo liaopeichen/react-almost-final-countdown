2c7dfcd979bc83366644c20014637f7ff5f6a6a3
liaopeichen <152478331+liaopeichen@users.noreply.github.com>
2024-01-17 17:19:43 +0800

    set up Player component initial functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function changeHandler(event) {
+    setSubmitted(false);
+    setPlayerName(event.target.value);
+  }
+
+  function clickHandler() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "unknown entity"}!</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={changeHandler} value={playerName} />
+        <button onClick={clickHandler}>Set Name</button>
       </p>
     </section>
   );
